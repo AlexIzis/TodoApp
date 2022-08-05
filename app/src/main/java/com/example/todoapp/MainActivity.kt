8eb7eba9f327687fc2ea8d88e784cf2c10fa28f5
Alexandr <aleksandr.izis@mail.ru>
2022-08-05 15:53:03 +0400

    dynamic task creation

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.todoapp
 
+import android.app.Activity
+import android.app.Instrumentation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.CalendarView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -24,7 +27,7 @@ class MainActivity : AppCompatActivity(){
         fullTodoList.add(Task(1,"1.7.2022", "1.7.2022", "14:00", "one", "hello"))
         fullTodoList.add(Task(2,"2.7.2022", "2.7.2022", "15:00", "two", "hello"))
         fullTodoList.add(Task(3,"2.7.2022", "2.7.2022", "14:00", "three", "hello"))
-        fullTodoList.add(Task(4,"1.7.2022", "1.7.2022", "12:00", "four", "hello"))
+        fullTodoList.add(Task(4,"1.7.2022", "1.7.2022", "12:31", "four", "hello"))
 
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
@@ -39,10 +42,23 @@ class MainActivity : AppCompatActivity(){
             startActivity(intent)
         }
 
+        val getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if (it.resultCode == Activity.RESULT_OK){
+                val id = fullTodoList.size + 1
+                val name = it.data?.getStringExtra("crName").toString()
+                val dateStart = it.data?.getStringExtra("crDate").toString()
+                val dateFinish = it.data?.getStringExtra("crDate").toString()
+                val time = it.data?.getStringExtra("crTime").toString()
+                val desc = it.data?.getStringExtra("crDesc").toString()
+                fullTodoList.add(Task(id, dateStart,dateFinish, time, name, desc))
+            }
+        }
+
         fab.setOnClickListener{
             val intent = Intent(this, MainActivity3::class.java)
-            startActivity(intent)
+            getResult.launch(intent)
         }
+
 
         calendar.setOnDateChangeListener { calendarView, year, month, dayOfMonth ->
             val curDate = "$dayOfMonth.$month.$year"
--- app/src/main/java/com/example/todoapp/MainActivity3.kt
+++ app/src/main/java/com/example/todoapp/MainActivity3.kt
@@ -1,11 +1,36 @@
 package com.example.todoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        val nameView: EditText = findViewById(R.id.editTextTextPersonName)
+        val descView: EditText = findViewById(R.id.editTextTextPersonName2)
+        val timeView: EditText = findViewById(R.id.editTextTime)
+        val dateView: EditText = findViewById(R.id.editTextDate)
+        val testView: TextView = findViewById(R.id.textView2)
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener{
+            val name = nameView.getText().toString()
+            val desc = descView.getText().toString()
+            val time = timeView.getText().toString()
+            val date = dateView.getText().toString()
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("crName", name)
+            intent.putExtra("crDesc", desc)
+            intent.putExtra("crTime", time)
+            intent.putExtra("crDate", date)
+            setResult(RESULT_OK, intent)
+            finish()
+        }
     }
 }
